3ccc3f9e72ca84d2aa53b65e55fc9ea4a3445cb3
JAEGEON LEE <worjs2964@gmail.com>
2022-08-09 13:27:43 +0900

    frist commit

---
+++ src/main/java/link/projectjg/apiserver/ApiShareServiceApplication.java
@@ -0,0 +1,13 @@
+package link.projectjg.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiShareServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiShareServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/link/projectjg/apiserver/ApiShareServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package link.projectjg.apiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiShareServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
