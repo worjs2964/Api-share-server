86475349d79284cb4ea7979b7d6df7f387291e2e
JAEGEON LEE <worjs2964@gmail.com>
2022-08-19 13:19:58 +0900

    refactor: MailController.java 삭제
    인증 메일 재전송 요청을 MemberController.java로 옮김

--- src/main/java/link/projectjg/apiserver/controller/MemberController.java
+++ src/main/java/link/projectjg/apiserver/controller/MemberController.java
@@ -16,6 +16,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.security.Principal;
 
 @Api(tags = {"member"})
 @RequiredArgsConstructor
@@ -41,6 +44,12 @@ public class MemberController {
     @ApiOperation(value = "인증 메일 확인", notes = "인증 메일을 확인용 요청입니다. 인증은 메일이 발송된 후 3분안에 완료해야 합니다.")
     public ResponseEntity<Response<String>> checkEmailToken(@RequestParam("token") String token, @RequestParam("email") String email) {
         return new ResponseEntity<>(Response.OK(memberService.authenticate(token, email)), HttpStatus.OK);
+    }
+
+    @PostMapping("/resend-authentication-mail")
+    @ApiOperation(value = "인증 메일 재전송 요청", notes = "인증 메일을 재전송할 수 있습니다. 단 인증메일은 아직 인증이 되지 않은 회원이 인증 메일을 보낸지 1분이 지나야 보낼 수 있습니다.")
+    public ResponseEntity<Response<String>> resendAuthenticationEmail(@ApiIgnore Principal principal) {
+        return new ResponseEntity<>(Response.OK(memberService.resendAuthenticationEmail(principal.getName())), HttpStatus.OK);
     }
 
 }
