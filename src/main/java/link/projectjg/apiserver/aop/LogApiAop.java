c179c058b5f7ae3fb7a8a58b71860c770b42dd2f
JAEGEON LEE <worjs2964@gmail.com>
2022-08-20 23:51:37 +0900

    feat: 로그 파일 저장 기능 추가
    운영환경에선 API 요청과 에러에 대한 로그를 파일로 저장
    MDC를 사용해서 쓰레드별 로그 구분

---
+++ src/main/java/link/projectjg/apiserver/aop/LogApiAop.java
@@ -0,0 +1,54 @@
+package link.projectjg.apiserver.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
+
+@Slf4j
+@Aspect
+@Component
+public class LogApiAop {
+
+    @Pointcut("execution(* link.projectjg.apiserver.controller..*.*(..))")
+    private void api(){}
+
+    @AfterReturning(value = "api()", returning = "returnObj")
+    public void afterReturnLog(JoinPoint joinPoint, Object returnObj) {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        MDC.put("traceId", UUID.randomUUID().toString().substring(0,13));
+        Method method = getMethod(joinPoint);
+        log.info("======= method name = {} =======", method.getName());
+        log.info("URI: ({}) {}", request.getMethod(), request.getRequestURI());
+
+        Object[] args = joinPoint.getArgs();
+        if (args.length <= 0) log.info("no parameter");
+        for (Object arg : args) {
+            log.info("parameter type = {}", arg.getClass().getSimpleName());
+            log.info("parameter value = {}", arg);
+        }
+
+        log.info("return type = {}", returnObj.getClass().getSimpleName());
+        log.info("return value = {}", returnObj);
+
+        MDC.clear();
+    }
+
+    // JoinPoint로 메서드 정보 가져오기
+    private Method getMethod(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        return signature.getMethod();
+    }
+}
