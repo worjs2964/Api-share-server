933037903e4f8477a06567ebdbd524b03e238fb1
JAEGEON LEE <worjs2964@gmail.com>
2022-08-24 16:33:22 +0900

    refactor: @ApiIgnore 추가
    사용자 기능이 아니라 swagger에서 제거

--- src/main/java/link/projectjg/apiserver/controller/HealthCheckController.java
+++ src/main/java/link/projectjg/apiserver/controller/HealthCheckController.java
@@ -3,7 +3,9 @@ package link.projectjg.apiserver.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
+@ApiIgnore
 @RestController
 @RequestMapping("/v1")
 public class HealthCheckController {
